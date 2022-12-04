c0cba5bc8819c510eb9c12e20986c63082760faf
Rafal Wisniewski <rafal.2.wisniewski@atos.net>
2022-12-04 09:16:35 +0100

    change BaseErrorResponse

--- src/main/java/travel/ways/travelwaysapi/_core/model/dto/BaseErrorResponse.java
+++ src/main/java/travel/ways/travelwaysapi/_core/model/dto/BaseErrorResponse.java
@@ -1,11 +1,22 @@
 package travel.ways.travelwaysapi._core.model.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
+
 public class BaseErrorResponse {
     private String message;
-    private int status;
+    private HttpStatus status;
+    private String timestamp;
+    private String path;
+
+    public BaseErrorResponse(String message, HttpStatus status, String path) {
+        this.message = message;
+        this.status = status;
+        this.timestamp = LocalDateTime.now().toString();
+        this.path = path;
+    }
 }
