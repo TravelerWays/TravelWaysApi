55bb77ee321bf04035d7e5006dce101dfc167935
Rafal Wisniewski <rafal.2.wisniewski@atos.net>
2022-12-05 13:29:12 +0100

    add validation

--- src/main/java/travel/ways/travelwaysapi/user/model/dto/request/CreateUserRequest.java
+++ src/main/java/travel/ways/travelwaysapi/user/model/dto/request/CreateUserRequest.java
@@ -4,13 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateUserRequest {
+    @NotEmpty
     private String name;
+    @NotEmpty
     private String surname;
+    @NotEmpty
     private String username;
+    @NotEmpty
     private String password;
+    @NotEmpty
+    @Email
     private String email;
 }
