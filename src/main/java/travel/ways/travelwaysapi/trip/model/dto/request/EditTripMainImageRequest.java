560ed15e6bca3ae99d8c0a575ab8d910f24dda80
Rafal Wisniewski <rafal.2.wisniewski@atos.net>
2023-03-11 11:56:59 +0100

    add tests

--- src/main/java/travel/ways/travelwaysapi/trip/model/dto/request/EditTripMainImageRequest.java
+++ src/main/java/travel/ways/travelwaysapi/trip/model/dto/request/EditTripMainImageRequest.java
@@ -1,12 +1,16 @@
 package travel.ways.travelwaysapi.trip.model.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EditTripMainImageRequest {
     @NotNull
     private String tripHash;
