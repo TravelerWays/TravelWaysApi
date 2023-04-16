3c6ca8ddad58f6377adf34cfa6ef616b47722b47
Rafal Wisniewski <rafal.2.wisniewski@atos.net>
2023-04-16 19:51:06 +0200

    fix formatting

--- src/main/java/travel/ways/travelwaysapi/trip/model/db/expense/CurrencyEnum.java
+++ src/main/java/travel/ways/travelwaysapi/trip/model/db/expense/CurrencyEnum.java
@@ -1,17 +1,18 @@
 package travel.ways.travelwaysapi.trip.model.db.expense;
-    public enum CurrencyEnum {
-        PLN(0),
-        EUR(1),
-        GBP(2),
-        USD(3);
 
-        private final int value;
+public enum CurrencyEnum {
+    PLN(0),
+    EUR(1),
+    GBP(2),
+    USD(3);
 
-        CurrencyEnum(int i){
+    private final int value;
+
+    CurrencyEnum(int i) {
         value = i;
-        }
-
-        public int getValue() {
-            return value;
-        }
     }
+
+    public int getValue() {
+        return value;
+    }
+}
